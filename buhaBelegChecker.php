ff71b5da7135d35e69b1c744b26044982d3c51e3
Andreas Döbeling <Kontakt@Doebeling.de>
2021-01-02 14:07:44 +0100

    Update buhaBelegChecker.php

--- buhaBelegChecker.php
+++ buhaBelegChecker.php
@@ -2,6 +2,8 @@
 
 namespace de\doebeling;
 
+use MimoGraphix\QIF\Parser;
+
 require_once '3rd-party/qif-library/src/Parser.php';
 
 class buhaBelegChecker
@@ -27,6 +29,11 @@ class buhaBelegChecker
      */
     const regExBelege = '/^#(?<id>[0-9]*)(_?(?<subid>[0-9]*)?)(?<desc>.*)(?<fileext>.pdf|.msg|.doc|.docx|.xls|.xlsx)$/';  //#?(?<belegId>[0-9]*)';
 
+    /**
+     * RegEx für Buchungstext
+     */
+    const regExBuchungstext = '/^#(?<id>[0-9]*).*$/';
+
 
     /**
      * @var array Array aller Belege
@@ -41,7 +48,9 @@ class buhaBelegChecker
     {
         // DEBUG
         //print_r(self::getDirRecursivelyAsArray(self::dirBelege));
-        $this->parseBelege();
+        //$this->parseBelege();
+        //$this->parseBuchungen();
+        $this->parseQif();
         print_r($this->b);
     }
 
@@ -63,12 +72,39 @@ class buhaBelegChecker
 
     private function parseQif($dir = self::dirQif)
     {
-
+        $files = self::getDirRecursivelyAsArray($dir);
+        foreach ($files as $filepath => $filename)
+        {
+            $file = file_get_contents($filepath);
+            $qifParser = new Parser( $file );
+            $qifParser->parse();
+            print_r($qifParser->getTransactions());
+            foreach( $qifParser->getTransactions() as $transaction )
+            {
+                // your code
+            }
+        }
     }
 
     private function parseBuchungen($dir = self::dirBuchungen)
     {
+        $files = self::getDirRecursivelyAsArray($dir);
+        foreach ($files as $filepath => $filename)
+        {
+            $buchungen = self::getCsvAsArray($filepath, ';');
+            foreach ($buchungen as $b)
+            {
+                // Bugfix for empty csv-lines
+                if (empty($b['Buchungsnummer'])) continue;
 
+                preg_match(self::regExBuchungstext, $b['Buchungstext'], $matches);
+                if (isset($matches['id']))               $b['id'] = $matches['id'];
+                else if (!empty($b['Belegnummer']))      $b['id'] = $b['Belegnummer'];
+                else                                     $b['id'] = '?';
+                $b['filename'] = $filepath;
+                $this->b[$b['id']]['buchungen'][] = $b;
+            }
+        }
     }
 
     private function getBAsMd()
@@ -107,6 +143,24 @@ class buhaBelegChecker
         return $result;
     }
 
+    /**
+     * @param $file
+     * @param string $delimeter
+     * @param string $eol
+     * @return array
+     */
+    static function getCsvAsArray($file, $delimeter = ',', $eol = PHP_EOL)
+    {
+        $r = array();
+        $f = preg_replace( '/[\x{200B}-\x{200D}\x{FEFF}]/u', '', file_get_contents($file));
+        $f = explode($eol, $f);
+        $h = str_getcsv(array_shift($f), $delimeter);
+        array_map('trim', $h); //Bugfix: Geschütztes Leerzeichen am Anfang des CSV-Files :-/
+        foreach ($f as $row => $line) foreach (str_getcsv($line, $delimeter) as $k => $v) $r[$row][$h[$k]] = $v;
+        return $r;
+    }
+
 }
 
 $bbc = new buhaBelegChecker();
+
