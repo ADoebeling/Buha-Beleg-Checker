d8ca5d914004aef9c558c1b401b9ef3d966af2a9
Andreas Döbeling <Kontakt@Doebeling.de>
2021-01-07 13:03:17 +0100

    Bugfix: array_key_first

--- doebeling.buhaBelegChecker.checker.class.php
+++ doebeling.buhaBelegChecker.checker.class.php
@@ -4,7 +4,7 @@ namespace DOEBELING\BuhaBelegChecker;
 setlocale (LC_ALL, 'de_DE@euro', 'de_DE', 'de', 'ge');
 
 //PHP7.2
-if (!function_exists(array_key_first))
+if (!function_exists('array_key_first'))
 {
     function array_key_first ($array) {
         foreach ($array as $a) return $a;
