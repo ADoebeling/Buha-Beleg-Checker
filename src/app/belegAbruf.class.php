a02ee9e83f777cf8a6cf235fec33ed912863d0ae
Andreas Döbeling <Kontakt@Doebeling.de>
2021-01-09 21:04:54 +0100

    Refactoring BelegAbruf

--- src/app/belegAbruf.class.php
+++ src/app/belegAbruf.class.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 namespace DOEBELING\buhaJournal;
+
+use Monolog\Handler\StreamHandler;
+
 error_reporting(E_ALL);
 
 require_once 'phpFunctionExtensions.php';
@@ -27,92 +30,149 @@ class belegAbruf
      */
     protected $belegMails;
 
-    public function __construct(string $sslHostname, int $sslPort, string $user, string $pwd)
+    /**
+     * @var log
+     */
+    public $log;
+
+    public function __construct(string $sslHostname, int $sslPort, string $user, string $pwd, array $log)
     {
-        $this->belegMails = new belegMails($sslHostname, $sslPort, $user, $pwd);
+        $this->setLog($log);
+        $this->log->debug(__METHOD__, [$sslHostname, $sslPort, $user]);
+        $this->belegMails = new belegMails($sslHostname, $sslPort, $user, $pwd, $log);
+    }
+
+    public function setLog(array $log)
+    {
+        $this->log = new log("belegAbruf");
+        foreach ($log as $handler)
+            $this->log->pushHandler($handler);
+        return $this;
     }
 
     public function downloadMailsAsEml($dir)
     {
-        foreach ($this->belegMails->getAll() as $m)
+        foreach ($this->belegMails->getAll() as &$m)
         {
             /** @var $m belegMail */
 
             // Dir
             $dir = sprintf($dir, $m->getDateAsY());
             if (!file_exists($dir))
+            {
                 mkdir($dir, 0770, true);
+            }
 
             // Filename
-            $fileName = [];
-            $fileName[] = getStringAsFilename($m->getFromDomain());
-            $fileName[] = $m->getUid();
-            $fileName[] = getStringAsFilename($m->getSubject());
-            $fileName[] = $m->getDateAsYMDString();
-            $fileName[] = $m->getUid();
-            $fileName = $dir . '/' . implode('__', $fileName) . '.eml';
-
-            file_put_contents($fileName, $m->getEml());
+            $fileName = $m->getFilePrefix()->filePrefix . getStringAsFilename($m->getSubject()).'.eml';
+            file_put_contents($dir.$fileName, $m->getEml());
+            $m->addDownloadFile($fileName, $dir.$fileName);
+            $this->log->info("Download $fileName", [__METHOD__]);
         }
         return $this;
     }
 
-    protected function downloadMailParts($dir, $subtype = 'PDF'): belegAbruf
+
+
+    public function downloadMailAttachments($dir): belegAbruf
     {
-        foreach ($this->belegMails->getAll() as $m)
+        foreach ($this->belegMails->getAll() as $belegMail)
         {
-            /** @var $m belegMail */
-            if (!empty($m->getAttachments()))
+            /** @var $belegMail belegMail */
+            foreach ($belegMail->getParts() as $part)
             {
-                // Dir
-                $dir = sprintf($dir, $m->getDateAsY());
-                if (!file_exists($dir))
-                    mkdir($dir, 0770, true);
-
-                foreach ($m->getAttachments($subtype) as $attachmentName => $attachementBody)
+                if ($part->subtype == 'PDF')
                 {
+                    // Dir
+                    $dir = sprintf($dir, $belegMail->getDateAsY());
+                    if (!file_exists($dir))
+                        mkdir($dir, 0770, true);
+
                     // Filename
-                    $fileName = [];
-                    $fileName[] = getStringAsFilename($m->getFromDomain());
-                    $fileName[] = $m->getUid();
-                    $fileName[] = getStringAsFilename($m->getSubject());
-                    $fileName[] = getStringAsFilename($attachmentName);
-                    $fileName[] = $m->getDateAsYMDString();
+                    $file = $belegMail->getFilePrefix()->filePrefix . getStringAsFilename($part->filename);
 
-                    $fileExt = explode('.', $attachmentName);
-                    $fileExt = end($fileExt);
-
-                    $fileName = $dir . '/' . implode('__', $fileName) . ".$fileExt";
-
-                    file_put_contents($fileName, $attachementBody);
+                    file_put_contents($dir.$file, base64_decode($part->body));
+                    $this->log->info("Download $file", [__METHOD__]);
                 }
             }
         }
         return $this;
     }
 
-    public function downloadPdfMailAttachments($dir): belegAbruf
+    /**
+     * Downloads Mails als MD and
+     *
+     * @param $dir
+     * @return $this
+     */
+    public function downloadMailsAsMd($dir): belegAbruf
     {
-        return $this->downloadMailParts($dir, 'PDF');
-    }
+        foreach ($this->belegMails->getAll() as $belegMail)
+        {
+            /** @var belegMail $belegMail */
+            $belegMail;
 
-    public function downloadMailsAsTxt($dir): belegAbruf
-    {
-        return $this->downloadMailParts($dir, 'PLAIN');
+            $metaText = '`' . getStringAsMd($belegMail->getFrom()) . '` *an* <br>';
+            $metaText .= '`' . getStringAsMd($belegMail->getTo()) . '` <br><br>';
+            $metaText .= '`' . getStringAsMd($belegMail->getSubject()) . '`';
+
+            $attachements = '';
+            foreach ($belegMail->getParts() as $partId => $part)
+            {
+                $attachements .= getMdLink($part->filename, $belegMail->getFilePrefix()->path . $belegMail->getFilePrefix()->filePrefix . $part->filename) . "<br>";
+            }
+
+            $body = "| E-Mail #{$belegMail->getUid()} |  |   |\n";
+            $body .= "|---|---|---|\n";
+            $body .= "| E-Mail: | $metaText | $attachements |\n";
+            $body .= "\n \n";
+            $body .= $belegMail->getBodyAsText();
+
+
+
+            // Dir
+            $dir = sprintf($dir, $belegMail->getDateAsY());
+            if (!file_exists($dir))
+                mkdir($dir, 0770, true);
+
+            // Filename
+            $file = $belegMail->getFilePrefix()->filePrefix . getStringAsFilename($belegMail->getSubject()) . '.md';
+
+            file_put_contents($dir.$file, $body);
+            $this->log->info("Download $file", __METHOD__);
+        }
+        return $this;
     }
 
     public function hasMails(): bool
     {
-        return $this->belegMails->hasMails();
+        $hasMail = $this->belegMails->hasMails();
+        if ($hasMail)
+        {
+            foreach ($this->belegMails->getAll() as $belegMail)
+            {
+                /** @var belegMail $belegMail */
+                $this->log->info("Mail gefunden: {$belegMail->getSubject()}", __METHOD__);
+            }
+        }
+        return $hasMail;
     }
 
     public function moveMailsToDir($dir): belegAbruf
     {
+        $this->log->debug(__METHOD__, [$dir]);
         foreach ($this->belegMails->getAll() as $m)
         {
             /** @var $m belegMail */
             $m->moveMailsTo($dir);
+            $this->log->info("Mail verschoben: {$m->getSubject()}", __METHOD__);
         }
         return $this;
+    }
+
+
+    public function __destruct()
+    {
+        $this->log->debug(__METHOD__);
     }
 }
