3fd2d445bb22c440fe202123f4881b8ebe7e5a19
Andreas Döbeling <Kontakt@Doebeling.de>
2021-01-17 19:37:15 +0100

    Buchungssätze werden ausgegeben

--- example.php
+++ example.php
@@ -8,6 +8,8 @@ require_once './src/app/journal.php';
 $journal = new journal();
 
 $journal->add(kontoauszug::load(['csvDir' => '../2020/KA - Kontoauszug/']));
+$journal->add(buchungssatz::load(['csvDir' => '../2020/BP - Buchungsprotokoll']));
+//$journal->add(pdf::load(['dir' => '../2020/ER - Eingangsrechnungen/']));
 $journal->debug();
 
 file_put_contents(".test.md", $journal->getMdTable()->getMd());
